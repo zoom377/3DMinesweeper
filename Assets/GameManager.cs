a83166d99630646817da8b511de3ad369d39a067
Tomas Evans <tomasevanscode@gmail.com>
2023-07-12 11:03:37 +0100

    3D array initialisation
    Add code that initialises a 3D array of tiles and creates visuals to represent them

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject tilePrefab;
+
+    const int width = 10, height = 10, depth = 10;
+
+    Tile[] tileArray;
+
     void Start()
     {
-
+        Reset();
     }
 
     void Update()
@@ -16,11 +22,82 @@ public class GameManager : MonoBehaviour
 
     private void Reset()
     {
-        
+        InitTileArray();
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int z = 0; z < depth; z++)
+                {
+                    UpdateTileVisual(new Vector3Int(x, y, z));
+                }
+            }
+        }
+    }
+
+    void InitTileArray()
+    {
+        tileArray = new Tile[width * height * depth];
+        for (int i = 0; i < tileArray.Length; i++)
+        {
+            if (tileArray[i] != null && !tileArray[i].Visual)
+            {
+                Destroy(tileArray[i].Visual);
+            }
+
+            tileArray[i] = new Tile { };
+        }
+    }
+
+    void UpdateTileVisual(Vector3Int pos)
+    {
+        var tile = tileArray[Array3DToIndex(pos)];
+        if (!tile.Visual)
+        {
+            //Create visual
+            var visual = Instantiate(tilePrefab, pos, Quaternion.identity);
+        }
+
+        //Update
+    }
+
+    int Array3DToIndex(Vector3Int pos)
+    {
+        return pos.x * height * depth
+            + pos.y * depth
+            + pos.x;
+    }
+
+    Vector3Int ArrayIndexTo3D(int index)
+    {
+        int z = index % depth;
+        z /= depth;
+
+        int y = index % height;
+        y /= height;
+
+        int x = index & width;
+        x /= width;
+
+        return new Vector3Int(x, y, z);
+    }
+
+    bool IsInBounds(Vector3Int pos)
+    {
+        if (pos.x > 0 &&
+            pos.y > 0 &&
+            pos.z > 0 &&
+            pos.x < width &&
+            pos.y < height &&
+            pos.z < depth)
+            return true;
+
+        return false;
     }
 
     class Tile
     {
-
+        public GameObject Visual;
+        public bool IsBomb, IsFlagged;
     }
 }
