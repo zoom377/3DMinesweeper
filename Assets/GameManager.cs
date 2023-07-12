1cc3e6ea11416d2312e423b1dc1431f66533764a
Tomas Evans <tomasevanscode@gmail.com>
2023-07-12 10:13:57 +0100

    Initial commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void Reset()
+    {
+        
+    }
+
+    class Tile
+    {
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# 3DMinesweeper
+ Minesweeper but 3D
